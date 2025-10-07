b6a2067c6cae2fde493635200703e3015b207226
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-10-08 00:27:45 +0800

    Added seat legend

--- public/reservation-form.js
+++ public/reservation-form.js
@@ -1,16 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('reservationForm');
-    const selectMealOptions = document.getElementById('mealOptions');
-    const checkBoxExtraBaggage = document.getElementById('extraBaggage');
+$(function() { //Note: Same as $(document).ready(function() {
+    const inputFirstName = $("#firstName");
+    const inputLastName = $("#lastName");
+    const inputEmail = $("#email");
+    const inputPassportNumber = $("#passportNumber");
+    const selectMealOptions = $("#mealOptions");
+    const checkBoxExtraBaggage = $("#extraBaggage");
+    const totalCostElement = $("#totalCost");
+    const btnSubmit = $("#submitBtn");
     
-    selectMealOptions.onchange = updateTotalCost;
-    checkBoxExtraBaggage.onchange = updateTotalCost;
-    
-
     function getTotalCost() {
-        var totalCost = 0;
+        const mealOptionValue = selectMealOptions.val().trim();
+        const extraBaggageValue = checkBoxExtraBaggage.is(":checked");
+        const baseCost = 500;
+        let totalCost = baseCost;
 
-        switch (selectMealOptions.value) {
+        switch (mealOptionValue) {
             case "standard":
                 totalCost += 50;
                 break;
@@ -23,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
             default:
         }
 
-        if (checkBoxExtraBaggage.checked) {
+        if (extraBaggageValue) {
             totalCost += 75;
         }
 
@@ -31,8 +35,34 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function updateTotalCost() {
-        const totalCostElement = document.getElementById('totalCost');
         const totalCost = getTotalCost();
-        totalCostElement.textContent = `Total Cost $${totalCost}`;
+        //totalCostElement.textContent = `Total Cost $${totalCost}`;
+        totalCostElement.text(`Total Cost $${totalCost}`);
     }
+
+    function onConfirm() {
+        const firstName = inputFirstName.val().trim();
+        const lastName = inputLastName.val().trim();
+        const email = inputEmail.val().trim();
+        const passportNumber = inputPassportNumber.val().trim();
+
+        let errors = [];
+        if (!firstName) errors.push("First name is required.");
+        if (!lastName) errors.push("Last name is required.");
+        if (!email) errors.push("Email is required.");
+        if (!passportNumber) errors.push("Passport number is required.");
+        if (errors.length > 0) {
+            // $message.html("<div class="alert alert-danger">" + errors.join("<br>") + "</div>");
+            return;
+        }
+
+        // $message.html("<div class="alert alert-success">Registration successful!(client-side only)</div>");
+        // $("#regForm")[0].reset();
+        alert("Submitted! (client-side)");
+    }
+
+    selectMealOptions.on("change", updateTotalCost);
+    checkBoxExtraBaggage.on("change", updateTotalCost);
+    btnSubmit.on("click", onConfirm);
+    updateTotalCost(); //Sets up on load    
 });
