e10429847c808a91d62dba6a6b1b0c4a593b86d5
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-11-08 17:37:54 +0800

    Fixed typo

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchmea = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     fullName:   {type: String, required: true},
     email:      {type: String, required: true}, 
     password:   {type: String, required: true},
