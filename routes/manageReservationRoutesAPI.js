3bb26f3c8a54f93a6bc1edd3d9dce07db5b01e08
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-11-23 16:39:38 +0800

    Removed sending an error when no reservations are found

--- routes/manageReservationRoutesAPI.js
+++ routes/manageReservationRoutesAPI.js
@@ -8,9 +8,11 @@ router.get('/:userId', async (req, res) => {
             user: req.params.userId
         });
 
+        /*
         if (!reservation) {
             return res.status(404).json({ error: 'Reservation not found' });
         }
+        */
         res.json(reservation);
     } catch (err) {
         res.status(500).json({ error: 'Server error' });
