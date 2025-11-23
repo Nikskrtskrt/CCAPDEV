7b065bffe8f68de536ac5391ea49f5c5129804a2
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-11-23 19:55:41 +0800

    Initializes the reservation

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
     const toastEl = $('#toastMsg');
     const toast = new bootstrap.Toast(toastEl[0]);
+    const reservationTableBody = $('#reservationTableBody');
 
     function showToast(msg, type = 'danger') {
         $('#toastMsg')
@@ -8,6 +9,61 @@ $(document).ready(() => {
             .addClass(`text-bg-${type}`);
         $('#toastText').text(msg);
         toast.show();
+    }
+
+    function initializeReservations(arrReservations, arrFlights) {
+        if (arrReservations.length == 0) {
+            return
+        }
+
+        reservationTableBody.empty();
+
+        for (i = 0; i < arrReservations.length; i++) {
+            const curReservation = arrReservations[i];
+            const curFlight = arrFlights.find(flight => flight._id == curReservation.flight)
+            console.log("Current Reservation: ", curReservation);
+            console.log("Current Flight: ", curFlight);
+            console.log("Current Reservation Id: ", curReservation._id);
+
+            const flightNo = curFlight.flightNo;
+            const flightDate = curFlight.date;
+            const seatNo = curReservation.seatNo;
+            const fareClass = curReservation.fareClass;
+            const totalPrice = curReservation.totalPrice;
+            const status = curReservation.status;
+            
+            const tr = $("<tr>")
+                .data("id", curReservation._id)
+                .append( $("<td>").append(flightNo) )
+                .append( $("<td>").append(flightDate) )
+                .append( $("<td>").append(seatNo) )
+                .append( $("<td>").append(fareClass) )
+                .append( $("<td>").append(totalPrice) )
+                .append( $("<td>").append(status) )
+                .append( $("<td>")
+                    .addClass("btn btn-sm btn-danger delete-reservation-btn")
+                    .append("Delete")
+                );
+            
+            reservationTableBody.append(tr);
+
+            /*
+            <tr data-id="{{_id}}">
+                <td>{{ flight.flightNo }}</td>
+                <td>{{ formatDate flight.date }}</td>
+                <td>{{ seatNo }}</td>
+                <td>{{ fareClass }}</td>
+                <td>{{ totalPrice }}</td>
+                <td>{{ status }}</td>
+                <td>
+                    <button class="btn btn-sm btn-danger delete-reservation-btn">Delete</button>
+                </td>
+            </tr>
+            */
+            
+        }
+
+
     }
 
     $(document).on('click', '.delete-reservation-btn', function () {
@@ -30,5 +86,16 @@ $(document).ready(() => {
         });
     });
 
+    $.ajax({
+        url: `/api/reservations/user`,
+        method: 'GET',
+        success: function (data) {
+            console.log(`Data Got:\n`, data);
 
+            initializeReservations(data.reservations, data.flights)
+        },
+        error: function (xhr) {
+            console.error('Error fetching reservation data:', xhr.responseText);
+        }
+    });
 });
