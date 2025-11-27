fb75c408bea5c3b9c7cf7afe5d62233546d9d6ae
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-11-27 12:35:25 +0800

    Added full test of AuthRoutes.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,42 +1,75 @@
 const request = require('supertest');
 const app = require('../server');
+const User = require('../models/User');
+const { baseModelName } = require('../models/Reservation');
 
-describe("Testing User Profile Actions", () => {
-    let agent;
+describe("Testing AuthRoutes (User) Routes", () => {
+    let userAgent;
+    let adminAgent;
+
+    const BASE_USER_INFO = {
+        firstName: "Her",
+        lastName: "Shey",
+        email: "testing@jest.com",
+        password: "iLoveSupertest",
+        passportNo: "ID0123456789ABCD",
+    }
 
     beforeAll(() => {
-        agent = request.agent(app);
+        userAgent = request.agent(app);
     });
     
-    /*
-    afterAll(() => {
+    
+    afterAll(async () => {
+        await User.findOneAndDelete(BASE_USER_INFO);
     });
-    */
-   
-    test("Testing log in with valid input", async () => { // Added async
+    
+    test("Log in with invalid input (not registered)", async () => {
+        const result = await userAgent
+            .post("/login")
+            .send(BASE_USER_INFO);
+
+        expect(result.statusCode).toBe(200); //200 because it sent a render to say invalid
+    });
+
+    test("Registering", async () => {
+        const result = await userAgent
+            .post("/register")
+            .send(BASE_USER_INFO)
+
+        expect(result.statusCode).toBe(302);
+        
+        const userExists = await User.findOne(BASE_USER_INFO);
+        expect(userExists.firstName ).toBe(BASE_USER_INFO.firstName);
+        expect(userExists.lastName  ).toBe(BASE_USER_INFO.lastName);
+        expect(userExists.email     ).toBe(BASE_USER_INFO.email);
+        expect(userExists.password  ).toBe(BASE_USER_INFO.password);
+        expect(userExists.passportNo).toBe(BASE_USER_INFO.passportNo);
+        expect(userExists.role      ).toBe("User");
+    });
+
+    test("Log in with valid input", async () => { // Added async
         const loginData = {
             email: "registering@sample.com",
             password: "1234"
         };
 
-        const result = await agent
-            .post("/login") // Added endpoint
+        const result = await userAgent
+            .post("/login")
             .send(loginData);
 
         //console.log("Redirect Status: ", result.status);
         expect(result.statusCode).toBe(302); //302 because redirect
+        expect(result.headers.location).toBe("/userDashboard");
     });
 
-    test("Testing log in with invalid input", async () => { // Added async
-        const loginData = {
-            email: "registering@sample.com",
-            password: "guessed"
-        };
+    test("Log out", async () => {
+        const result = await userAgent
+            .post("/logout")
+            .send()
+        
+        expect(result.statusCode).toBe(200);
+    })
 
-        const result = await agent
-            .post("/login") // Added endpoint
-            .send(loginData);
-
-        expect(result.statusCode).toBe(200); //200 because it sent a render to say invalid
-    });
+    //test("Editing Name")
 })
