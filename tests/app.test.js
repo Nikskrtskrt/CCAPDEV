8641be0715001853121e59b9df20c99f979f79ef
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-11-27 11:54:30 +0800

    Added initial test

---
+++ tests/app.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const app = require('../server');
+
+describe("Testing User Profile Actions", () => {
+    let agent;
+
+    beforeAll(() => {
+        agent = request.agent(app);
+    });
+    
+    /*
+    afterAll(() => {
+    });
+    */
+   
+    test("Testing log in", async () => { // Added async
+        const loginData = {
+            email: "registering@sample.com",
+            password: "1234"
+        };
+
+        const result = await agent
+            .post("/login") // Added endpoint
+            .send(loginData);
+
+        //console.log("Redirect Status: ", result.status);
+        expect(result.statusCode).toBe(302); //302 because redirect
+    });
+})
