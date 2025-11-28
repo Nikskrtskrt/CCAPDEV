bcbaa33523d22e3c636d7c06fc2077bf1c884feb
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-11-28 09:32:57 +0800

    Added more constans for tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -16,6 +16,22 @@ const BASE_USER_INFO = {
     passportNo: "ID0123456789ABCD",
 }
 
+const BASE_USER_INFO_2 = {
+    firstName: "Cad",
+    lastName: "Mury",
+    email: "love@jest.com",
+    password: "anotherSupertest",
+    passportNo: "ID0123456789LMNO",
+}
+
+const BASE_ADMIN_INFO = {
+    firstName: "Emmy",
+    lastName: "Nem",
+    email: "slimjimmy@wrap.com",
+    password: "pleaseStandUp",
+    passportNo: "ADMIN_ABRACADABRA",
+}
+
 const UPDATED_USER_INFO = {
     firstName: "Tobly",
     lastName: "Ron",
